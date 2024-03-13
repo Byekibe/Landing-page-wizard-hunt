dfe7d122266e23c5b8d8a086ade53eb882fe6615
Byekibe <kibetpeter95@gmail.com>
2024-03-14 02:28:32 +0300

    Add Hero component

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -1,9 +1,16 @@
+import arrowIcon from "../../assets/Icon.png";
+
 export default function Hero() {
   return (
-    <div className="hero-section mt-24">
-      <h1>Your Magic Wand for Instant Documentation</h1>
-      <h3>Turn ANY process into a simple step-by step Guide</h3>
-      <button>Install Wizardshot </button>
-    </div>
+    <section className="hero-section mt-24">
+      <h1 className="">Your Magic Wand for Instant Documentation</h1>
+      <h3 className="uppercase text-16 mb-2">
+        Turn ANY process into a simple step-by step Guide
+      </h3>
+      <button className="hero-btn flex items-center">
+        <span className="pr-2">Install Wizardshot</span>
+        <img src={arrowIcon} alt="" />
+      </button>
+    </section>
   );
 }
