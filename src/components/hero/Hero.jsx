63daccc4f3bdbff81ddf4f20ab8bcdc66cd8755f
Byekibe <kibetpeter95@gmail.com>
2024-03-13 12:37:19 +0300

    Add hero section component

---
+++ src/components/hero/Hero.jsx
@@ -0,0 +1,9 @@
+export default function Hero() {
+  return (
+    <div className="hero-section mt-24">
+      <h1>Your Magic Wand for Instant Documentation</h1>
+      <h3>Turn ANY process into a simple step-by step Guide</h3>
+      <button>Install Wizardshot </button>
+    </div>
+  );
+}
