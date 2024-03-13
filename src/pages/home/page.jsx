13e7cdb6acd7fd0b02df9e90869fd59191e05fcc
Byekibe <kibetpeter95@gmail.com>
2024-03-13 12:41:11 +0300

    Modify styles in index.css file

--- src/pages/home/page.jsx
+++ src/pages/home/page.jsx
@@ -1,10 +1,12 @@
 import Navbar from "../../components/navbar/Navbar";
 import Header from "../../components/header/Header";
+import Hero from "../../components/hero/Hero";
 
 export default function Home() {
   return (
     <>
       <Header />
+      <Hero />
     </>
   );
 }
