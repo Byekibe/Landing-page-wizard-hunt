0e7235c38b41115230818e4ee0f74e2096da1728
Byekibe <kibetpeter95@gmail.com>
2024-03-14 02:33:37 +0300

    Add components to home page

--- src/pages/home/page.jsx
+++ src/pages/home/page.jsx
@@ -1,12 +1,16 @@
 import Navbar from "../../components/navbar/Navbar";
 import Header from "../../components/header/Header";
 import Hero from "../../components/hero/Hero";
+import VideoSection from "../../components/videoSection/VideoSection";
+import About from "../../components/about/About";
 
 export default function Home() {
   return (
     <>
       <Header />
       <Hero />
+      <VideoSection />
+      <About />
     </>
   );
 }
